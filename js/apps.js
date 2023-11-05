ff0e45f7c915c29257702e1a581f67b1554f155f
samuelrenna <samuelrenna@gmail.com>
2023-11-05 12:57:24 -0300

    funcion mostrar informacion psj

--- js/apps.js
+++ js/apps.js
@@ -49,4 +49,47 @@ simuladorBtn.addEventListener("click", function() {
         }
     } while (personaje1 !== "perro" && personaje1 !== "gato");
 
+    function mostrarInformacionPersonaje(nombre, nombreUsuario, razaPersonaje) {
+        let mensaje = "Hola " + nombre + " aca te dejamos la informacion completa de tu cuenta \n\n";
+        mensaje += "Nombre de usuario: " + nombreUsuario + "\n";
+    
+        if (razaPersonaje === "bulldog") {
+            mensaje += "Estadísticas del perro bulldog:\n";
+            mensaje += " - Velocidad: 5\n";
+            mensaje += " - Fuerza: 8\n";
+            mensaje += " - Agilidad: 3\n";
+        } else if (razaPersonaje === "doberman") {
+            mensaje += "Estadísticas del perro doberman:\n";
+            mensaje += " - Velocidad: 7\n";
+            mensaje += " - Fuerza: 9\n";
+            mensaje += " - Agilidad: 5\n";
+        } else if (razaPersonaje === "poodle") {
+            mensaje += "Estadísticas del perro poodle:\n";
+            mensaje += " - Velocidad: 6\n";
+            mensaje += " - Fuerza: 4\n";
+            mensaje += " - Agilidad: 8\n";
+        } else if (razaPersonaje === "siames") {
+            mensaje += "Estadísticas del gato siamés:\n";
+            mensaje += " - Velocidad: 4\n";
+            mensaje += " - Fuerza: 3\n";
+            mensaje += " - Agilidad: 9\n";
+        } else if (razaPersonaje === "persa") {
+            mensaje += "Estadísticas del gato persa:\n";
+            mensaje += " - Velocidad: 3\n";
+            mensaje += " - Fuerza: 2\n";
+            mensaje += " - Agilidad: 7\n";
+        } else if (razaPersonaje === "montes") {
+            mensaje += "Estadísticas del gato montés:\n";
+            mensaje += " - Velocidad: 5\n";
+            mensaje += " - Fuerza: 5\n";
+            mensaje += " - Agilidad: 6\n";
+        } else {
+            mensaje += "Raza de personaje desconocida";
+        }
+    
+        alert(mensaje);
+    }
+    
+    mostrarInformacionPersonaje(nombreIngresado, nombreUsuario, razaPersonaje);
+
 });
